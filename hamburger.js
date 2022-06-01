35376890b6f17cdc8c1e259b751fce8c46f1f3ea
prabina.khanal@introcept.co <prabina.khanal@introcept.co>
2022-06-01 07:21:02 +0545

    changes in scroll bar and toggler

--- hamburger.js
+++ hamburger.js
@@ -1,19 +1,23 @@
+function noscroll(){
+    window.scrollTo(0,0);
+}
+
+window.addEventListener("scroll",noscroll)
+
 const back=document.querySelector('#back');
 const toogler=document.querySelector('#toogler');
-const Navbar=document.querySelector('#navbar');
+
 
 
 
 toogler.addEventListener('click',() =>{
-    toogler.classList.add('hidden');
     back.classList.remove('hidden')
-    Navbar.classList.remove('hidden')
+    toogler.classList.add('hidden')
+
     
 })
 
 back.addEventListener('click',() =>{
     back.classList.add('hidden');
     toogler.classList.remove('hidden')
-    Navbar.classList.add('hidden')
-    
 })
