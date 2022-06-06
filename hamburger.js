0719c54eb1573f74f85070ae923d5e98c73e6a19
prabina.khanal@introcept.co <prabina.khanal@introcept.co>
2022-06-06 22:15:31 +0545

    created closing effect while clicking outside div

--- hamburger.js
+++ hamburger.js
@@ -6,18 +6,28 @@ window.addEventListener("scroll",noscroll)
 
 const back=document.querySelector('#back');
 const toogler=document.querySelector('#toogler');
-
+const pinkClose=document.querySelector('#pinkclose');
+const overlay = document.getElementById('overlay');
 
 
 
 toogler.addEventListener('click',() =>{
     back.classList.remove('hidden')
     toogler.classList.add('hidden')
+    overlay.classList.remove('hidden')
 
-    
 })
 
 back.addEventListener('click',() =>{
     back.classList.add('hidden');
     toogler.classList.remove('hidden')
+    overlay.classList.add('hidden')
+
+})
+
+overlay.addEventListener('click' ,() =>{
+    back.classList.add('hidden')
+    overlay.classList.add('hidden')
+
+   
 })
